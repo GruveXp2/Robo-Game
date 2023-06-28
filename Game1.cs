9c93cdc2705d79f146290b0e07ad02f0d82b440b
GruveXp <gruveexp123@gmail.com>
2023-06-28 23:39:58 +0200

    Added player + movement
    The player is a tank, move by using wasd

--- Game1.cs
+++ Game1.cs
@@ -6,12 +6,15 @@ namespace RoboGame
 {
     public class Game1 : Game
     {
-        private GraphicsDeviceManager _graphics;
-        private SpriteBatch _spriteBatch;
+        private GraphicsDeviceManager graphics;
+        private SpriteBatch spriteBatch;
+
+        private Texture2D tankTexture;
+        private Vector2 playerPos;
 
         public Game1()
         {
-            _graphics = new GraphicsDeviceManager(this);
+            graphics = new GraphicsDeviceManager(this);
             Content.RootDirectory = "Content";
             IsMouseVisible = true;
         }
@@ -19,15 +22,17 @@ namespace RoboGame
         protected override void Initialize()
         {
             // TODO: Add your initialization logic here
+            playerPos = new Vector2 (graphics.PreferredBackBufferWidth / 2, graphics.PreferredBackBufferHeight / 2);
 
             base.Initialize();
         }
 
         protected override void LoadContent()
         {
-            _spriteBatch = new SpriteBatch(GraphicsDevice);
+            spriteBatch = new SpriteBatch(GraphicsDevice);
 
             // TODO: use this.Content to load your game content here
+            tankTexture = Content.Load<Texture2D>("tank");
         }
 
         protected override void Update(GameTime gameTime)
@@ -36,6 +41,21 @@ namespace RoboGame
                 Exit();
 
             // TODO: Add your update logic here
+            if (Keyboard.GetState().IsKeyDown(Keys.W))
+            {
+                playerPos.Y -= 2;
+            } else if (Keyboard.GetState().IsKeyDown(Keys.S))
+            {
+                playerPos.Y += 2;
+            }
+            if (Keyboard.GetState().IsKeyDown(Keys.A))
+            {
+                playerPos.X -= 2;
+            }
+            else if (Keyboard.GetState().IsKeyDown(Keys.D))
+            {
+                playerPos.X += 2;
+            }
 
             base.Update(gameTime);
         }
@@ -45,6 +65,9 @@ namespace RoboGame
             GraphicsDevice.Clear(Color.CornflowerBlue);
 
             // TODO: Add your drawing code here
+            spriteBatch.Begin();
+            spriteBatch.Draw(tankTexture, playerPos, null, Color.White, 0f, new Vector2(tankTexture.Width / 2, tankTexture.Height / 2), 0.2f, SpriteEffects.None, 0f);
+            spriteBatch.End();
 
             base.Draw(gameTime);
         }
