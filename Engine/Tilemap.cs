05c4f6a9fdf09e0576a7510a64e160084b0b0c6f
GruveXp <gruveexp123@gmail.com>
2023-06-29 23:06:19 +0200

    Adding tilemap
    Laying the foundation for the tilemap, which has tiles and entities in it, and a camera

---
+++ Engine/Tilemap.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RoboGame.Engine
+{
+    internal class Tilemap
+    {
+        // this will have code for the tilemap
+        // features:
+        // camera, when you play the camera will be the center of your screen, and when you move the camera,
+        //      the tilemap under will move on the screen until the camera is in the middle
+        // a list of entities that has a position on the tilemap, and entities inside the camera view will be drawn.
+        // tiles
+    }
+}
