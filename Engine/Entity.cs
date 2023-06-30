195ce547b0802fbe8390c6fcebbab6932d40d368
GruveXp <gruveexp123@gmail.com>
2023-07-01 00:18:40 +0200

    Adda entity class
    Man creater en entity med parametere som texture, posisjon og størrelse. så tar man bare  Entity.Draw() i draw funksjonen i main og så hjøres alt automatisk. man kan endre pos og størrelse ved å ta feks Entity.Pos.X = 69

---
+++ Engine/Entity.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace RoboGame.Engine
+{
+    internal class Entity // en sprite som har koordinat, teksturfil, størrelse, og kanskje id i framtida
+    {
+        public Vector2 Position { get; set; }
+        public Vector2 Size { get; set; }
+        private Texture2D Texture { get; set; }
+        public Entity(Texture2D texture, Vector2 pos, Vector2 size) 
+        {
+            Position = pos;
+            Texture = texture;
+            Size = size;
+        }
+
+        public virtual void Update() // gjør sånn at man kan adde stuff her om man vil, feks vil en player entity ha logic her for å bevege seg
+        {
+            
+        }
+
+        public void Draw(SpriteBatch spriteBatch) // ikke gjør dette om du har satt entitien på en tilemap (denne draw funksjonen tegner det rett på skjermen)
+        {
+            spriteBatch.Draw(Texture, Position, null, Color.White, 0f, new Vector2(Size.X / 2, Size.Y / 2), Size, SpriteEffects.None, 0f);
+        }
+    }
+}
